9247a612e379ae40af66f7d1e03462167a1b33ca
shikonga <wanjikungaruiya71@gmail.com>
2024-05-06 11:15:06 +0300

    Added footer/ weather.js

--- src/App.js
+++ src/App.js
@@ -1,88 +1,38 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import  AppDate  from './AppDate.js';
-import axios from "axios"
-import 'bootstrap/dist/css/bootstrap.css';
+import  Weather  from '.Weather.js';
 
 export default function App() {
-  let [city, setCity] = useState("Nairobi");
-  let[weatherData,setWeatherData] = useState({})
-  
-  function showCity(event){
-    setCity(event.target.value)
-  }
-
-  function handleResponse(response){
-    setWeatherData({
-   temperature: Math.round(response.data.main.temp),
-   city: response.data.name,
-   wind:response.data.wind.speed,
-   date: new Date(response.data.dt * 1000),
-   humidity: response.data.main.humidity,
-   description: response.data.weather[0].description,
- });
-}
-
-function handleSearch(event) {
-  event.preventDefault();
-  const cityNameInput = event.target.elements.city;
-  if (cityNameInput) {
-    const cityName = cityNameInput.value;
-    setCity(cityName);
-  } else {
-    console.error('City input element not found.');
-  }
-}
-  
-  
-  useEffect(function() {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bf54175800a55e59e6c4d6461deeef12&units=metric`;
-    axios.get(url).then(handleResponse)
-    .catch(error => {
-      console.error('Error fetching weather data:', error);
-    });
-  }, [city]);
-
-  
-  return (
-    <div className="WeatherApp">
-      <form onSubmit={handleSearch}>
-        <div className='row'>
-          <div className='col-9'>
-          <input type="text" placeholder="Enter a city.." className="Form-Control w-100 h-100" autoFocus onChange={showCity}/>
-          </div>
-          <div className='col-3'>
-             <input type="Submit" value="Search" className=" btn btn-primary"/>
-          </div>
-        </div>
-       
-      </form>
-      <h1>{weatherData.city}</h1>
-      <ul>
-        <li> <AppDate date={weatherData.date}/></li>
-        <li className='text-capitalize'>{weatherData.description}</li>
-      </ul> 
-      <div className='row'>
-      <div className='col-6'>
-      <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="weather-Newyork" />
-      <strong className='temperature'><span>{weatherData.temperature}</span></strong>
-      <span className='unit'>°C</span>
+  return(
+    <div className='App'>
+      <div className='container'>
+        <footer>
+        This project was coded by{" "}
+          <a
+            href="wanjikungaruiya71@gmail.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Wanjiku Ngaruiya
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
-      <div className='col-6'>
-        <ul>
-        <li>Precipitation 1%</li>
-        <li>Humidity: {weatherData.humidity}</li>
-        <li>Wind: {weatherData.wind}km/h</li>
-        </ul>
-      </div>
-      </div>
-
-    <div className='row'>
-
-
     </div>
-       
-    </div>
-  );
+  )
 }
-
